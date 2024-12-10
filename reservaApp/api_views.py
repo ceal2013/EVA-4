123f467cd98d9c790609cb0205e49cec7811a958
ceal2013 <carlos.ortiz51@inacapmail.cl>
2024-12-10 00:18:08 -0300

    Actualizar vista del dashboard de la API para redirigir a la página de inicio de sesión y mejorar la lógica de creación de reservas, incluyendo validaciones y actualización de disponibilidad de habitaciones.

--- reservaApp/api_views.py
+++ reservaApp/api_views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+from rest_framework import serializers
 from rest_framework import status
 from .models import Reserva, TipoHab
 from .serializers import ReservaSerializer
@@ -8,7 +9,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-@user_passes_test(lambda u: u.is_superuser)
+@user_passes_test(lambda u: u.is_superuser, login_url='login')
 def api_dashboard_view(request):
     # Lógica para mostrar la interfaz de la API
     return render(request, 'reservasApp/api_dashboard.html')
@@ -19,24 +20,21 @@ class ReservaListCreateAPIView(ListCreateAPIView):
     permission_classes = [IsAdminUser]  # Solo superusuarios pueden acceder
 
     def perform_create(self, serializer):
-        # Obtener datos del formulario
         tipo_hab = serializer.validated_data['tipoHab']
         fecha_entrada = serializer.validated_data['fecha_entrada']
         fecha_salida = serializer.validated_data['fecha_salida']
 
-        # Verificar disponibilidad
         if tipo_hab.cantidad < 1:
             raise serializers.ValidationError({'tipoHab': 'No hay disponibilidad para esta categoría de habitación.'})
 
-        # Calcular total según noches
         noches = (fecha_salida - fecha_entrada).days
         if noches < 1:
             raise serializers.ValidationError({'fecha_salida': 'La fecha de salida debe ser posterior a la fecha de entrada.'})
-        
-        total = noches * tipo_hab.tarifa
 
-        # Actualizar campos automáticos y guardar
-        serializer.save(total=total, status='R')
+        total = noches * tipo_hab.tarifa
+    
+        # Guarda la reserva
+        reserva = serializer.save(total=total, status='R')
         tipo_hab.cantidad -= 1
         tipo_hab.save()
 
