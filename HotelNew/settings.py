40422d1b3ac2a595c0308f1a58e3051a43e97f94
ceal2013 <carlos.ortiz51@inacapmail.cl>
2024-12-10 21:06:31 -0300

    Actualizar el código de idioma a español en la configuración del proyecto

--- HotelNew/settings.py
+++ HotelNew/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-Es'
+LANGUAGE_CODE = 'es-Es'
 
 TIME_ZONE = 'UTC'
 
